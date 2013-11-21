837b1f0926475aeb089fa3023163d659536fe245
Jason Miller <jason@millerjb.com>
2013-11-21 12:40:41 -0700

    install nvm

--- install-deps.sh
+++ install-deps.sh
@@ -1,19 +1,15 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
 
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-# sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
 # homebrew!
 # you need the code CLI tools YOU FOOL.
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
-# clone repo
-# git clone git://github.com/creationix/nvm.git ~/.nvm
+# nvm!
+git clone git://github.com/creationix/nvm.git ~/.nvm
 # # enable on terminal open
-# echo ". ~/.nvm/nvm.sh" >> ./.bashrc
+# echo ". ~/.nvm/nvm.sh" >> ./.bash_profile
 # # reopen your terminal and do the following
-# nvm install -s v0.10.3
+#nvm install v0.10.3
 # # set default node
-# nvm alias default v0.10.3
+#nvm alias default v0.10.3
