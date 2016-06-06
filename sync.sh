d795d4e8f4681486b0610209c1f51a1fb12510b7
Jason Miller <millerjb@hp.com>
2016-06-06 16:29:27 -0600

    explicit test mode

--- sync.sh
+++ sync.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-git pull
-function doIt() {
+function syncDotfiles() {
 	rsync --exclude ".git" --exclude ".DS_Store" --exclude "*.sh" --exclude "README.md" -a . ~
 }
-function testIt() {
+function testSyncDotfiles() {
 	rsync --exclude ".git" --exclude ".DS_Store" --exclude "*.sh" --exclude "README.md" -avn . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	syncDotfiles
+elif [ "$1" == "--test" -o "$1" == "-t" ]; then
+	testSyncDotfiles
 else
 	echo "This will overwrite existing files in your home directory:"
-	testIt
+	testSyncDotfiles
 	read -p "Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		echo "synchronizing files..."
-		doIt
+		syncDotfiles
 	fi
 fi
-unset doIt
+unset syncDotfiles
+source ~/.bash_profile
